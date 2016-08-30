ba18c7818ca28c3d39685350f831509202132cf9
John Stilley <thejollysin@gmail.com>
2016-08-30 10:35:11 -0700

    adding Very Simple game loop

--- mazes_of_erebor/main.cpp
+++ mazes_of_erebor/main.cpp
@@ -1,4 +1,5 @@
-
+#include <iostream>
+#include <string>
 #include "maze.h"
 
 
@@ -10,10 +11,28 @@ int main()
     int ncols = 31;
     int start[2] = {1, 1};
     int finish[2] = {1, 1};
+    string input;
 
-    backtracking_maze_gen(maze, MAX_SIZE, nrows, ncols);
-    gen_entrances_opposites(start, finish, nrows, ncols);
-    maze_print(maze, MAX_SIZE, nrows, ncols, start, finish);
+    while (true) {
+        // parse user input
+        getline(cin, input);
+        if (input == "q") {
+            break;
+        }
+
+        // generate a new maze
+        backtracking_maze_gen(maze, MAX_SIZE, nrows, ncols);
+        gen_entrances_opposites(start, finish, nrows, ncols);
+        maze_print(maze, MAX_SIZE, nrows, ncols, start, finish);
+
+        // increase the size of the maze (to a limit)
+        if (ncols < MAX_SIZE) {
+            ncols += 2;
+        }
+        if (nrows < (MAX_SIZE / 2)) {
+            nrows += 2;
+        }
+    }
 
     return 0;
 }
