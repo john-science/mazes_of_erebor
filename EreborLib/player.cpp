5683c1469f1e4a61a220620f644154c8255b335b
John Stilley <thejollysin@gmail.com>
2016-10-17 12:43:39 -0700

    player class placeholder

---
+++ EreborLib/player.cpp
@@ -0,0 +1,26 @@
+
+#include <random>
+#include <string>
+
+using namespace std;
+
+
+static const char* dwarf_names[] = {"Ai", "Alf", "Althin", "Althror", "Anar", "Andor", "Andwari",
+                                    "Andwarin", "Aurwang", "Austri", "Azagh", "Azaghal", "Azaghnin",
+                                    "Azaghnir", "Azaghnor", "Azalin", "Bafur", "Balin", "Bifur",
+                                    "Bilin", "Bofur", "Bombur", "Bruni", "Buri", "Dain", "Dofur",
+                                    "Dolgen", "Dori", "Dorin", "Draupnin", "Draupnir", "Duf",
+                                    "Dufin", "Durin", "Dwalin", "Fair", "Fid", "Fidnir", "Fili",
+                                    "Finn", "Floi", "Frar", "Fror", "Fundin", "Fundir", "Gamil",
+                                    "Gamlin", "Gingar", "Gloin", "Groin", "Grolin", "Gror", "Hanar",
+                                    "Har", "Harin", "Hepti", "Hornbori", "Hornbur", "Hrasin", "Hrasir",
+                                    "Hror", "Ingi", "Janir", "Jari", "Jarin", "Khorin", "Kili", "Korin",
+                                    "Kurin", "Lit", "Lofar", "Loni", "Modnir", "Modsognir", "Modsudrin",
+                                    "Modwitnir", "Nain", "Nali", "Nalin", "Nar", "Narvi", "Nidi", "Niping",
+                                    "Nordri", "Nori", "Nyi", "Nyir", "Nyirin", "Nyr", "Nyrad", "Oin",
+                                    "Ori", "Orin", "Radswid", "Rekk", "Sin", "Skafid", "Skirfir",
+                                    "Sognin", "Sognir", "Sudri", "Swid", "Telchar", "Thek", "Thorin",
+                                    "Thrain", "Throin", "Thror", "Vestri", "Vili", "Wali", "Wig", "Wigli",
+                                    "Wili", "Wirfir", "Wit"};
+
+// TODO: Insert PlayerData class
